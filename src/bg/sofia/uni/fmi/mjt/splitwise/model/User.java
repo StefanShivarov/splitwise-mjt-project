2a0c55234bf1e0dbc6052377a462175ab6b0a70b
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2024-01-29 01:38:28 +0200

    add user

---
+++ src/bg/sofia/uni/fmi/mjt/splitwise/model/User.java
@@ -0,0 +1,34 @@
+package bg.sofia.uni.fmi.mjt.splitwise.model;
+
+public class User {
+
+    private static long currentId = 1;
+    private final long id;
+    private final String username;
+    private final String hashedPass;
+
+    public User(String username, String hashedPass) {
+        this.username = username;
+        this.hashedPass = hashedPass;
+        this.id = currentId++;
+    }
+
+    public User(long id, String username, String hashedPass) {
+        this.id = id;
+        this.username = username;
+        this.hashedPass = hashedPass;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean checkPassword(String hashedPass) {
+        return this.hashedPass.equals(hashedPass);
+    }
+
+}
